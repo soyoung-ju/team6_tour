246c9b459485be6aa79a4e9e6c2577ca609958ef
Soyoung Ju <wtd98061888@gmail.com>
2024-06-08 22:48:53 +0900

    카드 컴포넌트 추가

--- local_trip.js
+++ local_trip.js
@@ -9,11 +9,79 @@
     let air_icon = document.querySelector(".air-icon")
     window.addEventListener('scroll', function(){
         let value = window.scrollY
-        console.log(value);
+        // console.log(value);
 
-        if(value<700){
+        if(value<900){
             air_icon.style.animation = "air-disappear 3s ease-out forwards"
         } else if(value>400){
             air_icon.style.animation = "air-slide 3s ease-out"
         }
     })
+
+
+    // 카드2
+    $("#card_a1").click(function() {
+        $("#card2_box_1").show();
+        $("#card2_box_2").hide();
+        $("#card2_box_3").hide();
+        $("#card2_box_4").hide();
+        $("#card_a1").addClass('underline');
+        $("#card_a2").removeClass('underline');
+        $("#card_a3").removeClass('underline');
+        $("#card_a4").removeClass('underline');
+    })
+    $("#card_a2").click(function() {
+        $("#card2_box_2").show();
+        $("#card2_box_1").hide();
+        $("#card2_box_3").hide();
+        $("#card2_box_4").hide();
+        $("#card_a2").addClass('underline');
+        $("#card_a1").removeClass('underline');
+        $("#card_a3").removeClass('underline');
+        $("#card_a4").removeClass('underline');
+    })
+    $("#card_a3").click(function() {
+        $("#card2_box_3").show();
+        $("#card2_box_1").hide();
+        $("#card2_box_2").hide();
+        $("#card2_box_4").hide();
+        $("#card_a3").addClass('underline');
+        $("#card_a1").removeClass('underline');
+        $("#card_a2").removeClass('underline');
+        $("#card_a4").removeClass('underline');
+    })
+    $("#card_a4").click(function() {
+        $("#card2_box_4").show();
+        $("#card2_box_1").hide();
+        $("#card2_box_2").hide();
+        $("#card2_box_3").hide();
+        $("#card_a4").addClass('underline');
+        $("#card_a1").removeClass('underline');
+        $("#card_a2").removeClass('underline');
+        $("#card_a3").removeClass('underline');
+    })
+
+    // 카드3
+    $("#card3_a1").click(function() {
+        $("#card3_box_1").show();
+        $("#card3_box_2").hide();
+        $("#card3_a1").addClass('underline');
+        $("#card3_a2").removeClass('underline');
+    })
+    $("#card3_a2").click(function() {
+        $("#card3_box_2").show();
+        $("#card3_box_1").hide();
+        $("#card3_a2").addClass('underline');
+        $("#card3_a1").removeClass('underline');
+    })
+
+    $( document ).ready(function() {
+        $("#card_a1").addClass('underline');
+        $("#card2_box_2").hide();
+        $("#card2_box_3").hide();
+        $("#card2_box_4").hide();
+
+        $("#card3_a1").addClass('underline');
+        $("#card3_box_2").hide();
+    });
+
