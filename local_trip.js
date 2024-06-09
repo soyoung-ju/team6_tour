83c988d6b2c140fdc5e52be2e0d26c4c6bf02219
Soyoung Ju <wtd98061888@gmail.com>
2024-06-09 16:07:49 +0900

    주간베스트 반응형 수정 및 가격, 버튼 색상 변경

--- local_trip.js
+++ local_trip.js
@@ -5,19 +5,42 @@
     touch: false,
     pause: "hover"      // hover 하면 슬라이드 재생 멈춤
     })
+    const carouselExample = document.querySelector('#carouselExample')
+    const carousel_2 = new bootstrap.Carousel(carouselExample, {
+        interval: 4000,     // 슬라이드 속도 지정
+        touch: false,
+        pause: "hover"      // hover 하면 슬라이드 재생 멈춤
+    })
 
-    let air_icon = document.querySelector(".air-icon")
+
+    // 비행기 애니메이션 스크롤 이벤트
+    let air_icon_right = document.querySelector(".air_icon_right")
+    // let air_icon_left = document.querySelector(".air_icon_left")
     window.addEventListener('scroll', function(){
         let value = window.scrollY
-        // console.log(value);
+        console.log(value);
 
         if(value<900){
-            air_icon.style.animation = "air-disappear 3s ease-out forwards"
-        } else if(value>400){
-            air_icon.style.animation = "air-slide 3s ease-out"
+            air_icon_right.style.animation = "air-disappear 3s ease-out forwards"
+        } else{
+            air_icon_right.style.animation = "air-slide 3s ease-out"
         }
     })
 
+    // 모바일 
+    $("#mobile_tab1").click(function() {
+        $("#mobile_box1").show();
+        $("#mobile_box2").hide();
+        $("#mobile_tab1").addClass('underline');
+        $("#mobile_tab2").removeClass('underline');
+    })
+
+    $("#mobile_tab2").click(function() {
+        $("#mobile_box2").show();
+        $("#mobile_box1").hide();
+        $("#mobile_tab2").addClass('underline');
+        $("#mobile_tab1").removeClass('underline');
+    })
 
     // 카드2
     $("#card_a1").click(function() {
@@ -65,14 +88,26 @@
     $("#card3_a1").click(function() {
         $("#card3_box_1").show();
         $("#card3_box_2").hide();
+        $("#card3_box_3").hide();
         $("#card3_a1").addClass('underline');
         $("#card3_a2").removeClass('underline');
+        $("#card3_a3").removeClass('underline');
     })
     $("#card3_a2").click(function() {
         $("#card3_box_2").show();
         $("#card3_box_1").hide();
+        $("#card3_box_3").hide();
         $("#card3_a2").addClass('underline');
         $("#card3_a1").removeClass('underline');
+        $("#card3_a3").removeClass('underline');
+    })
+    $("#card3_a3").click(function() {
+        $("#card3_box_3").show();
+        $("#card3_box_1").hide();
+        $("#card3_box_2").hide();
+        $("#card3_a3").addClass('underline');
+        $("#card3_a1").removeClass('underline');
+        $("#card3_a2").removeClass('underline');
     })
 
     $( document ).ready(function() {
@@ -83,5 +118,9 @@
 
         $("#card3_a1").addClass('underline');
         $("#card3_box_2").hide();
+        $("#card3_box_3").hide();
+
+        $("#mobile_tab1").addClass('underline');
+        $("#mobile_box2").hide();
     });
 
